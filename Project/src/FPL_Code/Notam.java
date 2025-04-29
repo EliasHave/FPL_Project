f30767a03e16d72300d7fbffcce105b53166c2d4
Haverinen Elias Viljami <elias.v.haverinen@student.jyu.fi>
2025-04-29 19:17:31 +0300

    Paranneltu notamien etsimistä, tehty erottelua ettei sekotu paikkojen notamit ja EN-ROUTEt yms

--- Project/src/FPL_Code/Notam.java
+++ Project/src/FPL_Code/Notam.java
@@ -4,6 +4,9 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * luokka jossa kerätään kaikki mahdollinen tieto liittyen esimerkiksi lähtö ja määränpää kenttien turvallisuudesta
  * Tiedot lähtee FlightPlannerille jossa niiden pohjalta jalostetaan lentosuunnitelma
@@ -31,17 +34,40 @@ public class Notam {
                     .get();
 
             StringBuilder notamBuilder = new StringBuilder();
+            StringBuilder notamBuilder1 = new StringBuilder();
+
+            Pattern pattern = Pattern.compile("Date:\\s*<b>(\\d{2}[A-Z]{3}\\d{4} \\d{4})</b>\\s*UTC");
+            Matcher matcher = pattern.matcher(doc.toString());
+            if (matcher.find()) {
+                String date = matcher.group(1);
+                notamBuilder.append("Tiedot haettu: " + date + " UTC \n");
+            }
 
             // Etsi kaikki h3-elementit (kenttien otsikot)
-            var headers = doc.select("h3");
+            var headers3 = doc.select("h3");
+            var h2 = doc.select("h2");
 
-            for (var header : headers) {
+            for (var header : headers3) {
                 String location = header.text().trim(); // Esim. "EFKI - KAJAANI"
                 notamBuilder.append("==== ").append(location).append(" ====\n");
 
                 // Etsi kaikki seuraavat taulukot tähän kenttään asti
                 var sibling = header.nextElementSibling();
-                while (sibling != null && !sibling.tagName().equals("h3")) {
+                while (sibling != null && !sibling.tagName().matches("h3|h2")) {
+                    if (sibling.tagName().equals("div") && sibling.select("table").size() > 0) {
+                        notamBuilder.append(sibling.text()).append("\n\n");
+                    }
+                    sibling = sibling.nextElementSibling();
+                }
+            }
+
+            for (var header : h2) {
+                String section = header.text().trim(); // Esim. "EN-ROUTE"
+                if ( !section.equals("AERODROMES") ) {
+                    notamBuilder.append("==== ").append(section).append(" ====\n");
+                }
+                var sibling = header.nextElementSibling();
+                while (sibling != null && !sibling.tagName().matches("h2|h3")) {
                     if (sibling.tagName().equals("div") && sibling.select("table").size() > 0) {
                         notamBuilder.append(sibling.text()).append("\n\n");
                     }
